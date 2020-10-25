ffb989773cb92f86328478b4efcc678fc79a0d86
DhanushkumarSivaji <dhanushkumar.s@yahoo.com>
2020-10-25 15:23:36 +0530

    Added video 2 lecture explanation - execution context

---
+++ namaste-js-akshay-saini/1-execution-context.js
@@ -0,0 +1,11 @@
+"Everything in javascript happens inside an Execution object."
+
+"Execurion context has two components"
+
+    "1. Memory - it is also called as Variable environment"
+        "This is where all the variables and functions are stored in an key value pair"
+
+    "2. Code - It is also called as Thread of execution"
+        "This is the place where code is excuted lie by line . One code at a time."
+
+"Javascript is a synchronous single threaded language - this means js can only execute one code at a time in a specific order. It will only go to next line when the current line execution completes."
